021b75a4b58cb12a4362a1acb5857eda0bca2572
Arthur Cendrier <arthur.cendrier@epitech.eu>
2021-07-24 17:33:41 +0200

    refactor(client-vue): remove unused components

--- client-vue/src/router/index.js
+++ client-vue/src/router/index.js
@@ -1,30 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import AuthorList from '../views/AuthorList.vue'
-import BlogPost from '../views/BlogPost.vue'
+import { AuthorList, BlogPost, Blog, User } from '@/views'
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    },
-  },
-  {
     path: '/blog',
     name: 'Blog',
-    component: function () {
-      return import('../views/Blog.vue')
-    },
+    component: Blog,
+    alias: ['/', '/posts'],
   },
   {
     path: '/posts/:id',
@@ -33,9 +15,7 @@ const routes = [
   },
   {
     path: '/users/:id',
-    component: function () {
-      return import('../views/User.vue')
-    },
+    component: User,
   },
   {
     path: '/authors',
---
+++ client-vue/src/views/index.js
@@ -0,0 +1,4 @@
+export { default as Blog } from './Blog'
+export { default as User } from './User'
+export { default as AuthorList } from './AuthorList'
+export { default as BlogPost } from './BlogPost'
