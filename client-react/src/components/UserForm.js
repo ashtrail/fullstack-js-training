efb72cedae5c9700344051e4cf82d8822547f904
Arthur Cendrier <arthur.cendrier@epitech.eu>
2021-07-28 18:25:46 +0200

    refactor(client-react): clean up UserForm with React Hook Form validation

--- client-react/src/components/UserForm.js
+++ client-react/src/components/UserForm.js
@@ -1,61 +1,39 @@
-import React, { useState } from 'react'
 import http from '../http-common'
 import { useForm } from 'react-hook-form'
+import { ErrorMessage } from '@hookform/error-message'
 
 export default function UserForm(props) {
-  const { register, reset, handleSubmit } = useForm()
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors, isValid, isDirty },
+  } = useForm({ mode: 'onChange' })
 
   // Check if we are creating a new user or editing an existing one
   const editingExistingUser = !!props.user
-
-  const currentUsernameErrorMessage = 'This is the current username'
-  const initialValidation = {
-    valid: false,
-    showHelp: editingExistingUser,
-    message: editingExistingUser ? currentUsernameErrorMessage : '',
-  }
-  const [validation, setValidation] = useState(initialValidation)
 
   const onClose = (event) => {
     event?.preventDefault?.()
     props.onClose?.()
   }
 
-  const clearForm = () => {
-    reset()
-    setValidation(initialValidation)
-  }
-
   const onSubmit = (data) => {
-    if (!validation.valid) return
     props.onSubmit?.({ name: data.name })
-    clearForm()
+    reset()
     onClose()
   }
 
-  const validate = async (value) => {
-    const res = await http.get('/users/available', {
-      params: { name: value },
-    })
-    let valid = false
-    let message = ''
-    if (value === '') {
-      message = 'User name cannot be empty'
-    } else if (value === props.user?.name) {
-      message = currentUsernameErrorMessage
-    } else if (!res.data?.available) {
-      message = 'This username is not available'
-    } else {
-      message = 'This username is available'
-      valid = true
-    }
-    setValidation({ valid, message, showHelp: true })
-  }
-
-  const helpClass = `help ${validation.valid ? 'is-success' : 'is-danger'}`
-  const helpMessage = <p className={helpClass}>{validation.message}</p>
-
-  const username = register('name')
+  const username = register('name', {
+    required: 'User name cannot be empty',
+    validate: {
+      notCurrentUsername: (value) =>
+        value !== props.user?.name || 'This is the current username',
+      available: async (value) =>
+        (await http.get('/users/available', { params: { name: value } }))?.data
+          ?.available || 'This username is not available',
+    },
+  })
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -63,27 +41,28 @@ export default function UserForm(props) {
         <label className="label">Name</label>
         <div className="control">
           <input
-            className={`input ${
-              validation.showHelp && !validation.valid ? 'is-danger' : ''
-            }`}
+            className={`input ${isDirty && !isValid ? 'is-danger' : ''}`}
             type="text"
             placeholder="John Doe"
             {...username}
-            onChange={(e) => {
-              username.onChange(e)
-              validate(e.target.value)
-            }}
             defaultValue={props.user?.name}
           />
         </div>
-        {validation.showHelp && helpMessage}
+        <ErrorMessage
+          errors={errors}
+          name="name"
+          render={({ message }) => <p className="help is-danger">{message}</p>}
+        />
+        {isValid && (
+          <p className="help is-success">This username is available</p>
+        )}
       </div>
 
       <div className="buttons">
         <button
           className="button is-primary"
           type="submit"
-          disabled={!validation.valid ? true : false}
+          disabled={!isValid ? true : false}
         >
           {editingExistingUser ? 'Edit' : 'Create'}
         </button>
